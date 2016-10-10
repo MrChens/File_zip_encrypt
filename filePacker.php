62eec4a06a530d8608d60b8f2fe0835b1b86e75d
ShiLong Chen <Mr_Chens@outlook.com>
2016-10-10 18:25:23 +0800

    init file zip encrypt

---
+++ filePacker.php
@@ -0,0 +1,120 @@
+<?php
+$iv = "here is your iv";
+$cryptKey = "here is your cryptKey";
+$cryptKey = substr($cryptKey,0,24);
+
+ $files = "";
+ $zipFile = "script.zip";
+ $finalFile = "v1";
+ for ($i=1; $i < count($argv) ; $i++) {
+     if ($argv[$i] == '-o') {
+         $finalFile = $argv[$i + 1];
+         break;
+     }
+     $files .=$argv[$i] . " ";
+ }
+
+ if (!empty($files)) {
+     //compress files
+     echo system("zip $zipFile $files")."\n";
+
+     //get and encrypt zip file's md5
+     $zipFileMD5 = md5_file($zipFile);
+     echo "script.zip Md5:$zipFileMD5\n";
+
+     $tripleDes = new STD3Des(base64_encode($cryptKey),base64_encode($iv));
+     $ret = $tripleDes->encrypt($zipFileMD5);
+     echo "tripledes:$ret\n";
+     if (!$ret) {
+         unlink($zipFile);
+         echo "fail to encrypt file md5\n";
+     }
+
+     $md5File = "key";
+     file_put_contents($md5File, $ret);
+
+     //pack script zip file and md5 file to final zip file
+     echo system("zip $finalFile $zipFile $md5File");
+
+     unlink($md5File);
+     unlink($zipFile);
+ }
+
+ class STD3Des
+ {
+     private $key = "";
+     private $iv = "";
+
+     /**
+     * 构造，传递二个已经进行base64_encode的KEY与IV
+     *
+     * @param string $key
+     * @param string $iv
+     */
+     function __construct ($key, $iv)
+     {
+         if (empty($key) || empty($iv)) {
+             echo 'key and iv is not valid';
+             exit();
+         }
+         $this->key = $key;
+         $this->iv = $iv;
+     }
+
+     /**
+     *加密
+     * @param <type> $value
+     * @return <type>
+     */
+     public function encrypt ($value)
+     {
+         $td = mcrypt_module_open(MCRYPT_3DES, '', MCRYPT_MODE_CBC, '');
+         $iv = base64_decode($this->iv);
+         $value = $this->PaddingPKCS7($value);
+         $key = base64_decode($this->key);
+         mcrypt_generic_init($td, $key, $iv);
+         $ret = base64_encode(mcrypt_generic($td, $value));
+         mcrypt_generic_deinit($td);
+         mcrypt_module_close($td);
+         return $ret;
+     }
+
+     /**
+     *解密
+     * @param <type> $value
+     * @return <type>
+     */
+     public function decrypt ($value)
+     {
+         $td = mcrypt_module_open(MCRYPT_3DES, '', MCRYPT_MODE_CBC, '');
+         $iv = base64_decode($this->iv);
+         $key = base64_decode($this->key);
+         mcrypt_generic_init($td, $key, $iv);
+         $ret = trim(mdecrypt_generic($td, base64_decode($value)));
+         $ret = $this->UnPaddingPKCS7($ret);
+         mcrypt_generic_deinit($td);
+         mcrypt_module_close($td);
+         return $ret;
+     }
+
+     private function PaddingPKCS7 ($data)
+     {
+         $block_size = mcrypt_get_block_size('tripledes', 'cbc');
+         $padding_char = $block_size - (strlen($data) % $block_size);
+         $data .= str_repeat(chr($padding_char), $padding_char);
+         return $data;
+     }
+
+     private function UnPaddingPKCS7($text)
+     {
+         $pad = ord($text{strlen($text) - 1});
+         if ($pad > strlen($text)) {
+             return false;
+         }
+         if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) {
+             return false;
+         }
+         return substr($text, 0, - 1 * $pad);
+     }
+ }
+?>
